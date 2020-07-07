46923f7bee55fa2b07533b9c78b6d7f0c69000b3
Dzhemil Suleimanov <jemil1931@gmail.com>
2020-07-07 09:49:44 +0300

    deleted not used elements, changed ItemList name

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from "react";
 
-export {App}
-
-export const elem = (<p style={{
-                      width: 300,
-                      height: 300,
-                      border: "3px solid black",
-                      fontSize: 20,
-                      color: "magenta",
-                      textAlign: "center"
-                    }}>"Hello World!"</p>);
-
 // Components
-const Element = () => (<ul>
+const ItemList = () => (<ul>
   <li>1</li>
   <li>2</li>
   <li>3</li>
   <li>4</li>
 </ul>)
+
 const App = () => (<div>
-      {Element()}
+      {ItemList()}
     </div>)
 
+export {App}
