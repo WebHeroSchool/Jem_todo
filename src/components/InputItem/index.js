d7d3d8fd15b1e318b9d53515a41863af91dff5fc
Dzhemil Suleimanov <jemil1931@gmail.com>
2020-07-08 23:43:36 +0300

    added material ui

--- src/components/InputItem/index.js
+++ src/components/InputItem/index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import TextField from '@material-ui/core/TextField';
 
-
-
-const InputItem = () => (<input/>);
+const InputItem = () => (<div>
+    <TextField
+        id="filled-secondary"
+        label="Добавить задачу"
+        color="primary"
+    />
+</div>);
 
 export default InputItem
